d9162745df75b9faaf0e820f1285da0844d4768c
alessandrasinigaglia <alessandra.sinigaglia06@gmail.com>
2025-02-14 10:24:41 +0100

    feat add about us

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,17 @@
 export default function AboutUs() {
     return (
         <>
-        <h1>about us</h1>
+        <div className="container">
+            <h1>Blog 138</h1>
+            <p>Benvenuti nel nostro angolo dedicato al gusto! Qui celebriamo la passione per il cibo con ricette autentiche, consigli culinari e storie che sanno di casa. Che tu sia un esperto ai fornelli o un semplice amante della buona cucina, troverai ispirazione per rendere ogni piatto un’esperienza speciale. Preparati a scoprire sapori, tradizioni e un pizzico di creatività!</p>
+            <section className="social">
+            <i class="fa-brands fa-youtube"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-pinterest"></i>
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-telegram"></i>
+            </section>
+        </div>
         </>
     )
 }
